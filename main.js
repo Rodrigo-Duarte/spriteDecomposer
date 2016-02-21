186e3273cf88e15f4fc390e8f094b48f40f2f441
Rodrigo Duarte <rduarte@thoughtworks.com>
2016-02-21 16:26:14 -0300

    Adding package.json

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 var http = require('http');
 var PNG = require('png-js');
 var decomposer = require('./decomposer');
+var fs = require('fs');
+var zpad = require('zpad');
+zpad.amount(5);
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {
@@ -11,22 +14,28 @@ var server = http.createServer(function(request, response) {
 
 
 });
-PNG.decode('ss.png', function(pixels) {
-	console.log(pixels);
-	var width = 360;
+var folder = "temp";
+var file = "ss.png";
+var png = PNG.load(file);
+png.decode(function(pixels) {
+	var width = png.width;
 	var alphamap = decomposer.getAlphaMap(pixels, width);
 	var vert = decomposer.verticalCut(alphamap);
 	var horz = decomposer.horizontalCut(alphamap);
 	var rgbam = decomposer.toRGBAMatrix(pixels, width);
+	fs.mkdir(folder);
+	var i = 0;
 	for (var h = 0; h < horz.length; h += 2) {
 		for (var v = 0; v < vert.length; v += 2) {
 			var a = [horz[h], vert[v]];
 			var b = [horz[h+1], vert[v+1]];
 			var crop = decomposer.crop(rgbam, a, b);
-			decomposer.save('f' + h + v + '.png', crop);
+			decomposer.save(folder + '/f' + zpad(i++) + '.png', crop);
 		}
 	}
+	console.log("done");
 });
+
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
 
