068ecfcafe87c4ba88044e1d74fcd0d913b71d0e
Rodrigo Duarte <rduarte@thoughtworks.com>
2016-02-21 15:48:16 -0300

    First run with real image

--- main.js
+++ main.js
@@ -11,9 +11,21 @@ var server = http.createServer(function(request, response) {
 
 
 });
-PNG.decode('px.png', function(pixels) {
+PNG.decode('ss.png', function(pixels) {
 	console.log(pixels);
-	console.log(decomposer.getAlphaMap(pixels, 20));
+	var width = 360;
+	var alphamap = decomposer.getAlphaMap(pixels, width);
+	var vert = decomposer.verticalCut(alphamap);
+	var horz = decomposer.horizontalCut(alphamap);
+	var rgbam = decomposer.toRGBAMatrix(pixels, width);
+	for (var h = 0; h < horz.length; h += 2) {
+		for (var v = 0; v < vert.length; v += 2) {
+			var a = [horz[h], vert[v]];
+			var b = [horz[h+1], vert[v+1]];
+			var crop = decomposer.crop(rgbam, a, b);
+			decomposer.save('f' + h + v + '.png', crop);
+		}
+	}
 });
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
