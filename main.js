356a0c9a4ade9d5b858f92582d241838bc2ce6ed
Rodrigo Duarte <rduarte@thoughtworks.com>
2016-02-21 18:01:32 -0300

    Go horse for index

--- main.js
+++ main.js
@@ -5,39 +5,66 @@ var decomposer = require('./decomposer');
 var fs = require('fs');
 var zpad = require('zpad');
 zpad.amount(5);
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function(request, response) {
-	response.writeHead(200, {
-		"Content-Type": "text/plain"
-	});
-	response.end("Hello World\n");
+var formidable = require('formidable');
+var zipFolder = require('zip-folder')
 
-
-});
-var folder = "temp";
-var file = "ss.png";
-var png = PNG.load(file);
-png.decode(function(pixels) {
-	var width = png.width;
-	var alphamap = decomposer.getAlphaMap(pixels, width);
-	var vert = decomposer.verticalCut(alphamap);
-	var horz = decomposer.horizontalCut(alphamap);
-	var rgbam = decomposer.toRGBAMatrix(pixels, width);
-	fs.mkdir(folder);
-	var i = 0;
-	for (var h = 0; h < horz.length; h += 2) {
-		for (var v = 0; v < vert.length; v += 2) {
-			var a = [horz[h], vert[v]];
-			var b = [horz[h+1], vert[v+1]];
-			var crop = decomposer.crop(rgbam, a, b);
-			decomposer.save(folder + '/f' + zpad(i++) + '.png', crop);
+function bla(filename, res) {
+	console.log('loading ' + filename);
+	var png = PNG.load(filename);
+	var folder = filename.substring(filename.lastIndexOf('/')+1);
+	png.decode(function(pixels) {
+		var width = png.width;
+		var alphamap = decomposer.getAlphaMap(pixels, width);
+		var vert = decomposer.verticalCut(alphamap);
+		var horz = decomposer.horizontalCut(alphamap);
+		var rgbam = decomposer.toRGBAMatrix(pixels, width);
+		fs.mkdir(folder);
+		var i = 0;
+		for (var h = 0; h < horz.length; h += 2) {
+			for (var v = 0; v < vert.length; v += 2) {
+				var a = [horz[h], vert[v]];
+				var b = [horz[h+1], vert[v+1]];
+				var crop = decomposer.crop(rgbam, a, b);
+				decomposer.save(folder + '/f' + zpad(i++) + '.png', crop);
+			}
 		}
-	}
-	console.log("done");
-});
+		console.log("done breaking... zipping");
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+		zipFolder(folder,folder + '.zip', function() {
+			console.log("done zipping");	
+			var stream = fs.createReadStream(folder + '.zip');
+			res.setHeader('Content-disposition', 'attachment; filename=' + folder + '.zip');
+			res.writeHead(200);
+			stream.pipe(res);
+
+		});
+		
+	});
+}
+
+http.createServer(function(req, res) {
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload 
+    var form = new formidable.IncomingForm();
+ 		var tnc = [];
+    form.parse(req, function(err, fields, files) {
+      console.log(files.upload.path);
+    	bla(files.upload.path, res);
+    });
+
+    return;
+  }
+ 
+  // show a file upload form 
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(8080);
 
 // Put a friendly message on the terminal
 console.log("Server running at http:127.0.0.1:8000/");
