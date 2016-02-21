2475f3c306285186f7371debf06213bbf8bc796b
Rodrigo Duarte <rduarte@thoughtworks.com>
2016-02-20 23:40:58 -0300

    Decomposer initial tests

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var server = http.createServer(function(request, response) {
 });
 PNG.decode('px.png', function(pixels) {
 	console.log(pixels);
-	console.log(decomposer.getAlphaMap(pixels));
+	console.log(decomposer.getAlphaMap(pixels, 20));
 });
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
